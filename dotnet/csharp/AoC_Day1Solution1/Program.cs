4cfffe1c03e262f3695ae36ac7e7126c6698c4d6
Armin Belsner <armin.belsner@gmail.com>
2019-11-24 16:46:43 +0100

    Dotnet solution for the AoC Day 1

---
+++ dotnet/csharp/AoC_Day1Solution1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+//using System.Linq;
+
+namespace AoC_Day1Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>();
+            using ( StreamReader streamReader = new StreamReader("AoC_Day1Input.txt"))
+            {
+                while (streamReader.Peek() >=0){
+                    Numbers.Add(Convert.ToInt32(streamReader.ReadLine()));
+                }
+            }
+            int sum = 0;
+            foreach (var item in Numbers)
+            {
+                sum += item;
+            }
+ //           int sum = Numbers.Sum(); // System.Linq necessary 
+            Console.WriteLine(sum);           
+        }
+    }
+}
