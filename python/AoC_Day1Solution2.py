bed015f1754fd5856889a8d0beb0f3b4555654c2
Armin Belsner <armin.belsner@gmail.com>
2019-11-24 14:32:08 +0100

    slow solution

---
+++ python/AoC_Day1Solution2.py
@@ -0,0 +1,24 @@
+zahlen = []
+
+with open("AoC_Day1Input.txt") as f:
+    for zeile in f:
+        zahlen.append(int(zeile.strip()))
+
+result = 0
+results = []
+found = False
+numberLoops = 0
+while not found:
+    numberLoops += 1
+    print(f"Loop #:{numberLoops}")
+    for zahl in zahlen:
+        result += zahl
+        if result in results:
+            print(f"Frequncy: {result}")
+            found = True
+            break
+        else:
+            results.append(result)
+print(f"Sum of the input: {sum(zahlen)}")
+# number of loops 144
+#frequency: 71961
